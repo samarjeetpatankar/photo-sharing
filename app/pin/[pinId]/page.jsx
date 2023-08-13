bd99984ab3af37e1dcea57f186a10f21f2511094
samarjeetpatankar <samarjeetpatankar@gmail.com>
2023-08-13 15:53:34 +0530

    crud

--- app/pin/[pinId]/page.jsx
+++ app/pin/[pinId]/page.jsx
@@ -2,7 +2,7 @@
 import React, { useEffect, useState } from "react";
 import PinImage from "./../../components/PinDetail/PinImage";
 import PinInfo from "./../../components/PinDetail/PinInfo";
-import { doc, getDoc, getFirestore } from "firebase/firestore";
+import { deleteDoc, doc, getDoc, getFirestore } from "firebase/firestore";
 import app from "@/app/Shared/firebaseConfig";
 import { HiArrowSmallLeft } from "react-icons/hi2";
 import { useRouter } from "next/navigation";
@@ -22,24 +22,42 @@ function PinDetail({ params }) {
       console.log("No such document!");
     }
   };
+
+  const deletePin = async () => {
+    try {
+      const docRef = doc(db, "pinterest-post", params.pinId);
+      await deleteDoc(docRef);
+      console.log("Document successfully deleted!");
+      router.push("/");
+    } catch (error) {
+      console.error("Error deleting document: ", error);
+    }
+  };
+
   return (
     <>
       {pinDetail ? (
         <div className=" bg-white p-3 md:p-12 rounded-2xl md:px-24 lg:px-36">
           <HiArrowSmallLeft
             className="text-[60px] font-bold ml-[-50px] 
-       cursor-pointer hover:bg-gray-200 rounded-full p-2 "
+            cursor-pointer hover:bg-gray-200 rounded-full p-2 "
             onClick={() => router.back()}
           />
           <div
             className="grid grid-cols-1 lg:grid-cols-2 md:gap-10 shadow-lg
-      rounded-2xl p-3 md:p-7 lg:p-12 xl:pd-16 "
+           rounded-2xl p-3 md:p-7 lg:p-12 xl:pd-16 "
           >
             <PinImage pinDetail={pinDetail} />
             <div className="">
               <PinInfo pinDetail={pinDetail} />
             </div>
           </div>
+          <button
+            className="bg-red-500 text-white px-4 py-2 rounded-md mt-4"
+            onClick={deletePin}
+          >
+            Delete
+          </button>
         </div>
       ) : null}
     </>
@@ -47,3 +65,5 @@ function PinDetail({ params }) {
 }
 
 export default PinDetail;
+
+
