1e8b8abc7f27125836cd9da65732fc007ca3508e
samarjeetpatankar <samarjeetpatankar@gmail.com>
2023-08-13 11:10:27 +0530

    user page

--- app/[userId]/page.jsx
+++ app/[userId]/page.jsx
@@ -1,12 +1,31 @@
-import React from 'react'
+"use client";
+import { doc, getFirestore } from "firebase/firestore";
+import React, { useEffect } from "react";
+import app from "../shared/firebaseConfig";
 
-const Profile = () => {
-  return (
-    <div>
-      Profile
-      47.46
-    </div>
-  )
-}
+const Profile = ({ params }) => {
+  const db = getFirestore(app);
 
-export default Profile
+  useEffect(() => {
+    console.log(params.userId.replace("%40", "@"));
+    if (params) {
+      getUserInfo(params.userId.replace("%40", "@"));
+    }
+  }, [params]);
+
+  const getUserInfo = async (email) => {
+    const docRef = doc(db, "user", email);
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+      setUserInfo(docSnap.data());
+    } else {
+      // docSnap.data() will be undefined in this case
+      console.log("No such document!");
+    }
+  };
+
+  return <div>Profile 47.46</div>;
+};
+
+export default Profile;
