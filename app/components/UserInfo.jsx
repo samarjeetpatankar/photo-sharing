73e03a6a904b0ac8b8dd172ff171a71f70447f2b
samarjeetpatankar <samarjeetpatankar@gmail.com>
2023-08-13 11:36:05 +0530

    user info and ceate form

--- app/components/UserInfo.jsx
+++ app/components/UserInfo.jsx
@@ -1,12 +1,29 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
 
-function UserInfo({userInfo}) {
-    console.log(userInfo);
+function UserInfo({ userInfo }) {
+  console.log(userInfo);
   return (
-    <div>
-      UserInfo
+    <div className="flex flex-col items-center">
+      <Image
+        src={userInfo.userImage}
+        alt="userImage"
+        width={100}
+        height={100}
+        className="rounded-full"
+      />
+
+      <h2 className="text-[30px] font-semibold">{userInfo.userName}</h2>
+      <h2 className="text-gray-400">{userInfo.email}</h2>
+
+      <button
+        className="bg-gray-200
+         p-2 px-3 font-semibold mt-5 rounded-full"
+      >
+        Share
+      </button>
     </div>
-  )
+  );
 }
 
-export default UserInfo
+export default UserInfo;
