5b287c1f688285afc201eee6a551fe339e26c325
samarjeetpatankar <samarjeetpatankar@gmail.com>
2023-08-13 13:09:53 +0530

    login and logout

--- app/components/UserInfo.jsx
+++ app/components/UserInfo.jsx
@@ -1,8 +1,16 @@
-import React from "react";
 import Image from "next/image";
+import React from "react";
+import { signOut, useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 function UserInfo({ userInfo }) {
   console.log(userInfo);
+  const router = useRouter();
+  const { data: session } = useSession();
+  const onLogoutClick = () => {
+    signOut();
+    router.push("/");
+  };
   return (
     <div className="flex flex-col items-center">
       <Image
@@ -13,15 +21,30 @@ function UserInfo({ userInfo }) {
         className="rounded-full"
       />
 
-      <h2 className="text-[30px] font-semibold">{userInfo.userName}</h2>
-      <h2 className="text-gray-400">{userInfo.email}</h2>
-
-      <button
-        className="bg-gray-200
-         p-2 px-3 font-semibold mt-5 rounded-full"
+      <h2
+        className="text-[30px]
+        font-semibold"
       >
-        Share
-      </button>
+        {userInfo.userName}
+      </h2>
+      <h2 className="text-gray-400">{userInfo.email}</h2>
+      <div className="flex gap-4">
+        <button
+          className="bg-gray-200
+         p-2 px-3 font-semibold mt-5 rounded-full"
+        >
+          Share
+        </button>
+        {session?.user.email == userInfo.email ? (
+          <button
+            className="bg-gray-200
+         p-2 px-3 font-semibold mt-5 rounded-full"
+            onClick={() => onLogoutClick()}
+          >
+            Logout
+          </button>
+        ) : null}
+      </div>
     </div>
   );
 }
